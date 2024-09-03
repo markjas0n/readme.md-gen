4831f83428baaa0aa7b670108cab4a46770e7126
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-02 20:18:00 -0500

    import packages index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer'); // Inquirer package to prompt user for input
+const fs = require('fs'); // File system module to write files
+const generateMarkdown = require('./utils/generateMarkdown'); // Function to generate markdown content for README
 // TODO: Create an array of questions for user input
 const questions = [];
 
