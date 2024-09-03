25689a5accbaf2aa59b5d73b6130a48172e0460d
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-02 20:19:14 -0500

    array of questions index.js

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,54 @@ const inquirer = require('inquirer'); // Inquirer package to prompt user for inp
 const fs = require('fs'); // File system module to write files
 const generateMarkdown = require('./utils/generateMarkdown'); // Function to generate markdown content for README
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?', // Prompt for project title
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of your project:', // Prompt for project description
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?', // Prompt for installation instructions
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide usage information:', // Prompt for usage information
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines?', // Prompt for contribution guidelines
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions?', // Prompt for test instructions
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:', // Prompt for license choice
+      choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'], // List of license options
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?', // Prompt for GitHub username
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?', // Prompt for email address
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
