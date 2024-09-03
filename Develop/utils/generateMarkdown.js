5627323c0ef16dda1ef902f56913d5329bdb8826
markjas0n@icloud.com <markjas0n@icloud.com>
2024-09-02 20:28:23 -0500

    genmarkdown.js update

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseBadge(license) {
+  if (license !== 'None') { // Check if a license is selected
+    return `![License](https://img.shields.io/badge/License-${license}-yellow.svg)`; // Return the badge URL
+  }
+  return ''; // Return an empty string if no license is selected
 }
 
-module.exports = generateMarkdown;
